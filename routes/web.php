de80796a3f4e7d588efdc79b38b1bb8859a20bff
Zane-ZZE <zane.zelenina@gmail.com>
2019-06-17 16:48:24 +0300

    Test GIT push

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// comment test
 
 Route::get('/all-products', function () { //example.com/all-products
     return view('all-products');
